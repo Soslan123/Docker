7a775870bd8df9425b458a2bf7985f5f391a85c6
Soslan Tuaev <c12420642@mydit.ie>
2014-10-16 14:40:07 +0000

    v1

--- python-flask/my_application/app.py
+++ python-flask/my_application/app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask import request
+from werkzeug import secure_filename
+from flask import make_response
 app = Flask(__name__)
 
 @app.route("/")
@@ -12,5 +14,20 @@ def upload_file():
         f = request.files['file']
         f.save('./uploads/'+f.filename)
     return '',201
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['the_file']
+        f.save('/var/www/uploads/' + secure_filename(f.filename))
+        
+
+@app.route('/')
+def index():
+    resp = make_response(render_template(...))
+    resp.set_cookie('username', 'the username')
+    return resp
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
