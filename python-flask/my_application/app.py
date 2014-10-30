2bb16452f64eca8d508e08bc1d03ad7ac40c9882
Soslan Tuaev <c12420642@mydit.ie>
2014-10-30 17:11:17 +0200

    changed to 202

--- python-flask/my_application/app.py
+++ python-flask/my_application/app.py
@@ -13,6 +13,6 @@ def upload_file():
     if request.method == 'POST':
         f = request.files['file']
         f.save('./uploads/'+f.filename)
-    return '',201
+    return '',202
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
